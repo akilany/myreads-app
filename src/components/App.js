365ea92ff060f3afa71bd39e88f1f34af7f1a4b0
akilany <ammyass2@gmail.com>
2022-08-20 16:11:37 +0200

    implement react-router-dom

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import "../App.css";
+// import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ListBooks from "./ListBooks";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route exact path="/" element={<ListBooks />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
